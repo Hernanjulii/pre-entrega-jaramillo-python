bd7319b3363979d0ed4555514269aee9fe66c901
hernanjulio <hernanjuliojaramillo@gmail.com>
2024-06-25 20:34:30 -0300

    cambios

---
+++ main.py
@@ -0,0 +1,7 @@
+from mi_paquete.modulo1 import Persona
+from mi_paquete.modulo2 import llamado_modulo2
+
+persona1 = Persona("Valentina", "Bonnefon")
+
+print(persona1)
+llamado_modulo2()
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="mi_paquete",  # Nombre del paquete
+
+    version="1.0",  # Versión del paquete
+
+    description="Estamos haciendo el primer paquete distribuible", #Una breve descripción del paquete
+
+    author="Clase 14 coder - python", #nombre del autor
+
+    author_email="coder@coder.com", #nombre del correo
+ 
+    packages=["mi_paquete"], # Lista de paquetes incluidos en la distribución
+)
